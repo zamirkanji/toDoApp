85487dff57bc432cb28c4e98db24c5eacca70860
Zamir Kanji <zamirkanji@comcast.net>
2020-03-28 11:41:32 -0700

    update 1 3.28.20

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
 let input = document.getElementById("input");
 let button = document.getElementById("enter");
 let ul = document.querySelector("ul");
-let listItem = document.querySelectorAll("li");
+let li = document.querySelectorAll("li");
+// let newli = document.querySelector("newli");
 const container = document.querySelector(".body-container");
-// let listBox = document.querySelector(".list-box");
+const clear = document.getElementsByClassName(".clear-all");
 
-// let listItemsTest = listItem; // ???
 
 function inputLength() {
   return input.value.length;
 }
 
 function createListElement() {
-  let listItem = document.createElement("li");
-  // let allListItems = listItem.querySelectorAll("li");
-  listItem.appendChild(document.createTextNode(input.value));
-  ul.appendChild(listItem);
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
   input.value = "";
 }
-
-function showListBox() {
-  ul.classList.add("list-Box");
-}
-
-// function createBtnElement() {
-// 	let btn = document.createElement("BUTTON");
-// 	btn.innerHTML = "Delete Item";
-// 	// btn.className = "delete-btn";
-// 	allListItems.appendChild(btn);
-// }
 
 function addListAfterClick() {
   if (inputLength() > 0) {
@@ -37,6 +25,7 @@ function addListAfterClick() {
     // createBtnElement(); //?
   }
 }
+
 function addListAfterEnter(e) {
   if (inputLength() > 0 && e.keyCode === 13) {
     createListElement();
@@ -45,14 +34,34 @@ function addListAfterEnter(e) {
   }
 }
 
-// Event Listeners (callback functions)
-button.addEventListener("click", addListAfterClick);
-input.addEventListener("keypress", addListAfterEnter);
+function removeAll() {
+  li.innerHTML = "";
+  newli.innerHTML = "";
+}
 
-for (let i = 0; i < listItem.length; i++) {
-  listItem[i].addEventListener("click", function () {
-    listItem[i].classList.toggle("done");
+for (let i = 0; i < li.length; i++) {
+  li[i].addEventListener("click", function () {
+    li[i].classList.toggle("done");
   });
 }
 
+// for (let i = 0; i < newli.length; i++) {
+//   newli[i].addEventListener("click", function () {
+//     newli[i].classList.toggle("done");
+//   });
+// }
+
+// Event Listeners (callback functions)
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterEnter);
+clear.addEventListener("click", removeAll);
+
+
 //new function to when li is created (input.value.length > 1) create button next to list item and when button is clicked it deletes said list item
+
+// function createBtnElement() {
+// 	let btn = document.createElement("BUTTON");
+// 	btn.innerHTML = "Delete Item";
+// 	// btn.className = "delete-btn";
+// 	allListItems.appendChild(btn);
+// }
