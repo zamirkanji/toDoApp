f6a080c148ae195bc4cce866bf69f35a9a019213
Zamir Kanji <zamirkanji@comcast.net>
2020-04-03 12:00:06 -0700

    update 1 april 3rd before i work on delete btn working and dark mode

--- script2.js
+++ script2.js
@@ -1,6 +1,3 @@
-// FUNCTIONS HAVE THEIR OWN SCOPE!!!!!
-
-
 let input = document.getElementById("input");
 let enter = document.getElementById("enter");
 let ol = document.querySelector("ol");
@@ -8,111 +5,57 @@ let li = document.querySelectorAll("li");
 const container = document.querySelector(".body-container");
 let clear = document.getElementById("clear-all");
 let listBox = document.querySelector(".list-box");
-// let listBtn = document.querySelector(".list-btn");
+
 
 
 function inputLength() {
   return input.value.length;
 }
 
+
 function createListElement() {
-
-
   let listItemDiv = document.createElement("div");
-  // buttonDiv.setAttribute("class", "button-div");
   listItemDiv.className = "button-div";
 
   let li = document.createElement("li");
   li.className = "list-item cross-out";
-
   li.appendChild(document.createTextNode(input.value));
-
   input.value = "";
-
   listItemDiv.appendChild(li);
 
   let deleteBtn = document.createElement("BUTTON");
   deleteBtn.innerHTML = "Delete";
   deleteBtn.className = "delete-btn";
-
-  // deleteBtn.onload.style.visibility = "hidden";
-  // li.onclick = document.querySelector(".delete-btn").style.visibility = "visible";
-  // li.appendChild(deleteBtn);
-
   li.appendChild(deleteBtn);
 
   document.querySelector("ol").appendChild(listItemDiv);
-  // document.querySelector("ol").appendChild(li);
-  // document.querySelector("ol").appendChild(deleteBtn);
-
   listItemDiv.appendChild(deleteBtn);
-
-
-
-
-
-
-  // li.onclick = createDeleteBtn;
-
-
-
-
-  // deleteBtn.addEventListener("click", deleteItem);
-  // deleteBtn.onclick = delete_row();
 }
-
-// function createDeleteBtn() {
-//   // let deleteBtn = document.createElement("BUTTON");
-//   // deleteBtn.innerHTML = "Delete";
-//   // deleteBtn.className = "delete-btn";
-//   // deleteBtn.onload.style.visibility = "hidden";
-//   // li.appendChild(deleteBtn);
-
-//   // listBox.appendChild(deleteBtn);
-
-//   // let x = document.querySelector(".delete-btn");
-//   // let o = document.querySelector(".list-box")
-//   // if (x.style.display === "none") {
-//   //   x.style.display = "block";
-//   //   o.appendChild(deleteBtn);
-//   // } else {
-//   //   x.style.display = "none";
-//   // }
-
-
-// }
-
-
-// function delete_row() {
-//   let listItem = this.parentElement;
-//   let ol = listItem.parentElement;
-//   ol.removeChild(listItem);
-// }
 
 
 function addListAfterClick() {
   if (inputLength() > 0) {
     createListElement();
-    // createBtnElement(); //?
+    // showClearBtn();
   }
 }
 
 function addListAfterEnter(e) {
   if (inputLength() > 0 && e.keyCode === 13) {
     createListElement();
-    // createDeleteBtn();
+
+    console.log(e);
   }
 }
-
 
 
 ol.addEventListener("click", (event) => {
   if (event.target.matches("li")) {
     event.target.classList.toggle("done");
     event.target.classList.toggle("black");
+
+    console.log(event);
   }
-
-
 });
 
 
@@ -121,17 +64,43 @@ function deleteItem() {
   deleteBtn.parentElement = "";
 }
 
+
+
 function removeAll() {
   li.innerHTML = "";
   ol.innerHTML = "";
 }
 
-clear.addEventListener("click", removeAll);
+function showClearBtn() {
+  let show = document.getElementById("clear-all");
+  show.style.visibility = "visible";
+}
 
-// Event Listeners (callback functions)
+function hideClearBtn() {
+  let hide = document.getElementById("clear-all");
+  hide.style.visibility = "hidden";
+}
+
+
+clear.addEventListener("click", removeAll);
+clear.addEventListener("click", hideClearBtn);
+
 enter.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterEnter);
+enter.addEventListener("click", showClearBtn);
+input.addEventListener("keypress", showClearBtn);
 
 
 
+function delete_row(e) {
+  // let listItem = this.parentElement;
+  // let ol = listItem.parentElement;
+  // ol.removeChild(listItem);
 
+  let listItemRow = document.querySelector(".button-div");
+  listItemRow.innerHTML = "";
+  console.log(e);
+}
+
+
+deleteBtn.addEventListener("click", delete_row);
